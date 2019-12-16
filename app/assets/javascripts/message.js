921ceaa8a36af24d620e33e1cacdb05920b26437
ynext-tech <yuji.next1017@gmail.com>
2019-12-16 18:42:01 +0900

    fix1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -45,7 +45,7 @@ $(function(){
         $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
       })
       .fail(function() {
-        console.log('error');
+        alert('error');
       });
     })
   }
